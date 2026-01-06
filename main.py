1f921b69d01a279b77939b6430b75d832315b76d
TeeKay <caleb@fhc.ca>
2026-01-06 14:03:24 -0500

    Added sys and Books

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from stats import num_words
+import sys
+from stats import num_words, num_characters, chars_dict_to_sorted_list
+
+if len(sys.argv) != 2:
+       print("Usage: python3 main.py <path_to_book>")
+       sys.exit(1)
 
 def get_book_text(filepath):
         with open(filepath) as f:
@@ -7,8 +12,17 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(sys.argv[1])
     num_words_in_book = num_words(book_text)
+    num_chars_in_book = num_characters(book_text)
+    sorted_list = chars_dict_to_sorted_list(num_chars_in_book)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words_in_book} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_list:
+           print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
 
 main()               
--- stats.py
+++ stats.py
@@ -3,3 +3,22 @@ def num_words(text):
     for num in text.split():
         word_count += 1
     return word_count
+
+def num_characters(text):
+    char_count = {}
+    for ch in text:
+        ch = ch.lower()
+        char_count[ch] = char_count.get(ch, 0) + 1
+    return char_count
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        if ch.isalpha():
+            sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+    
+
+def sort_on(items):
+    return items["num"]
